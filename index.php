dc6152034f74d66f07feac67579e27caaed89f50
Ángel Alemán <146956164+AngelAleman15@users.noreply.github.com>
2025-09-05 00:14:51 +0000

    agregue todo

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Cajero ATM</title>
+</head>
+<body>
+    <div class="body-atm">
+        <div class="displaycontainer-atm">
+            <div class="displaybtn-atm" style="top: 55px; left: 20px;">
+                <div class="btn-atm"></div>
+                <div class="btn-atm"></div>
+                <div class="btn-atm"></div>
+                <div class="btn-atm"></div>
+            </div>
+            <div class="screen-atm"></div>
+            <div class="displaybtn-atm" style="top: 55px; right: 20px;">
+                <div class="btn-atm"></div>
+                <div class="btn-atm"></div>
+                <div class="btn-atm"></div>
+                <div class="btn-atm"></div>
+            </div>
+        </div>
+
+        <div class="componentscontainer-atm">
+            <div class="cardreader-atm"></div>
+            <div class="keypad-atm">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button style="background-color: yellow; width: 4vh;"></button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button style="background-color: red; width: 4vh;"></button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button style="background-color: green; width: 4vh;"></button>
+                <button></button>
+                <button>0</button>
+                <button></button>
+                <button style="width: 4vh;"></button>
+            </div>
+            <div class="paperscontainer-atm">
+                <div class="cashdispenser-atm"></div>
+                <div class="receiptprinter-atm"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
